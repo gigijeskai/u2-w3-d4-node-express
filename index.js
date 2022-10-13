f413d59a74418b6a14eb1078fa8eacc3688e5647
Luigi Indaco <luigindaco@gmail.com>
2022-10-13 20:41:02 +0200

    consegna compito

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ async function mostraCarte() {
 
   let carteTesto = await arrayMagic.text();
   console.log(carteTesto);
+  let tutteLeCarte = JSON.parse(carteTesto).cards;
+  document.querySelector("#container").innerHTML = "";
+  for (carta of tutteLeCarte) {
+    document.querySelector("#container").innerHTML += `<p>${carta.name}</p>`;
+  }
 }
 
 window.onload = () => {
+  document.querySelector("#container").innerHTML = "loading...";
   mostraCarte();
 };
